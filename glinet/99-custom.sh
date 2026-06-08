f58421d062720e0b5e2269e083dff324a40f95d7
wangjontao <54728149+wangjontao@users.noreply.github.com>
2026-06-08 19:01:46 +0800

    Update 99-custom.sh

--- glinet/99-custom.sh
+++ glinet/99-custom.sh
@@ -1,40 +1,80 @@
 #!/bin/sh
 
-set -x
 exec >/tmp/99-custom.log 2>&1
+set -x
 
-LOGFILE="/tmp/99-custom.log"
+echo "========== 99-custom START $(date) =========="
 
-echo "===== 99-CUSTOM START $(date) ====="
-
-# Hostname
 #################################################
 
-uci set system.@system[0].hostname='DulWiFi'
+# Hostname
+
+#################################################
+
+uci set system.@system[0].hostname='DulWiFi-TK'
 uci commit system
 
 #################################################
-# Root密码
+
+# Root Password
+
 #################################################
 
+if command -v chpasswd >/dev/null 2>&1; then
 echo 'root:password' | chpasswd
+fi
 
 #################################################
+
+# LAN IP
+
+#################################################
+
+uci set network.lan.ipaddr='192.168.9.1'
+uci set network.lan.netmask='255.255.255.0'
+uci commit network
+
+#################################################
+
 # WiFi
+
 #################################################
 
-for iface in $(uci show wireless | grep "=wifi-iface" | cut -d. -f2 | cut -d= -f1)
+IDX=0
+
+while uci -q get wireless.@wifi-iface[$IDX] >/dev/null 2>&1
 do
-    uci set wireless.$iface.disabled='0'
-    uci set wireless.$iface.ssid='A'
-    uci set wireless.$iface.encryption='psk2'
-    uci set wireless.$iface.key='a1111111'
+
+```
+DEVICE=$(uci -q get wireless.@wifi-iface[$IDX].device)
+
+uci set wireless.@wifi-iface[$IDX].disabled='0'
+uci set wireless.@wifi-iface[$IDX].encryption='psk2'
+uci set wireless.@wifi-iface[$IDX].key='a1111111'
+
+case "$DEVICE" in
+    radio0)
+        uci set wireless.@wifi-iface[$IDX].ssid='DulWiFi-2.4G'
+    ;;
+    radio1)
+        uci set wireless.@wifi-iface[$IDX].ssid='DulWiFi-5G'
+    ;;
+    *)
+        uci set wireless.@wifi-iface[$IDX].ssid='DulWiFi'
+    ;;
+esac
+
+IDX=$((IDX+1))
+```
+
 done
 
 uci commit wireless
 
 #################################################
-# 固件信息
+
+# Firmware Description
+
 #################################################
 
 cat >/etc/openwrt_release <<EOF
@@ -48,48 +88,65 @@ DISTRIB_TAINTS=''
 EOF
 
 #################################################
-# NPS
+
+# NPS Auto Config
+
 #################################################
 
 if [ -f /etc/config/nps ] && [ -f /etc/init.d/nps ]; then
 
-    sleep 5
+```
+sleep 5
 
-    WAN_DEV=$(uci -q get network.wan.device)
+WAN_DEV=$(uci -q get network.wan.device)
 
-    [ -z "$WAN_DEV" ] && WAN_DEV=$(ip route | awk '/default/ {print $5; exit}')
+[ -z "$WAN_DEV" ] && \
+WAN_DEV=$(ip route | awk '/default/ {print $5; exit}')
 
-    WAN_MAC=$(cat /sys/class/net/$WAN_DEV/address 2>/dev/null)
+WAN_MAC=$(cat /sys/class/net/$WAN_DEV/address 2>/dev/null)
 
-    if [ -n "$WAN_MAC" ]; then
+echo "WAN_DEV=$WAN_DEV"
+echo "WAN_MAC=$WAN_MAC"
 
-        NPS_KEY=$(echo "$WAN_MAC" | tr -d ':' | tr '[:lower:]' '[:upper:]')
+if [ -n "$WAN_MAC" ]; then
 
-        uci set nps.@nps[0].enabled='1'
-        uci set nps.@nps[0].server_addr='47.83.9.208'
-        uci set nps.@nps[0].server_port='8024'
-        uci set nps.@nps[0].protocol='tcp'
-        uci set nps.@nps[0].compress='1'
-        uci set nps.@nps[0].crypt='1'
-        uci set nps.@nps[0].vkey="$NPS_KEY"
+    NPS_KEY=$(echo "$WAN_MAC" | tr -d ':' | tr '[:lower:]' '[:upper:]')
 
-        uci commit nps
+    echo "NPS_KEY=$NPS_KEY"
 
-        /etc/init.d/nps enable
-        /etc/init.d/nps restart
+    uci set nps.@nps[0].enabled='1'
+    uci set nps.@nps[0].server_addr='47.83.9.208'
+    uci set nps.@nps[0].server_port='8024'
+    uci set nps.@nps[0].protocol='tcp'
+    uci set nps.@nps[0].compress='1'
+    uci set nps.@nps[0].crypt='1'
+    uci set nps.@nps[0].vkey="$NPS_KEY"
 
-    fi
+    uci commit nps
+
+    /etc/init.d/nps enable
+    /etc/init.d/nps restart
+fi
+```
 
 fi
 
 #################################################
-# 应用配置
+
+# Apply
+
 #################################################
+
+/etc/init.d/network restart
+
+sleep 3
 
 wifi reload
 
+sync
+
 logger -t 99-custom "DulWiFi init success"
 
-echo "===== 99-CUSTOM END ====="
+echo "========== 99-custom END $(date) =========="
 
 exit 0
